9d9f1a5d78db9f803fa3c62c41b7063179a0fdfc
Timy007 <40737347+Timy007@users.noreply.github.com>
2018-08-31 19:06:59 +0200

    Add files via upload

---
+++ example_pie_1.py
@@ -0,0 +1,8 @@
+"""Simple example pie using standard settings"""
+
+from musical_plot_pie import musical_plot_pie
+from musical_plot_hex import musical_plot_hex
+
+location = r'C:\Users\Timothy\Schubert\data.csv'
+
+musical_plot_pie(location)
