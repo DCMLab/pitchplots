fd3e69595fbcbf8ba2ff287346adb91d2dd50e52
Timothy Loayza <timothy.loayza@hotmail.com>
2018-10-24 22:36:49 +0200

    Delete example_pie_1.py

--- example_pie_1.py
+++
@@ -1,8 +0,0 @@
-"""Simple example pie using standard settings"""
-
-from musical_plot_pie import musical_plot_pie
-from musical_plot_hex import musical_plot_hex
-
-location = r'C:\Users\Timothy\Schubert\data.csv'
-
-musical_plot_pie(location)
