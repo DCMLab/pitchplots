bf5b50e8032801f63322fddb0f06fecaa9470e1c
Timothy Loayza <timothy.loayza@hotmail.com>
2019-03-23 10:36:23 +0100

    update parser, why did you add this line?

--- parser.py
+++ parser.py
@@ -18,7 +18,6 @@ class ParseError(Exception):
 
 ### DEFINE PARSER
 def xml_to_csv(filepath=os.path.dirname(os.path.realpath(__file__))+'\\'+'data_example.mxl',
-def xml_to_csv(filepath=os.path.dirname(os.path.realpath(__file__))+'\\'+'data_example.mxl',
                filename=None, save_csv=True, duration='whole_note'):
     """return the Dataframe, and possbily register it in csv, of the musicxml file
     
